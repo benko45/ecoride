9b946a2a0f0bacbf67f556f6b9f50be82a0a9be2
Benoît Vicente <benoit.vicente@hotmail.fr>
2025-02-24 23:27:39 +0100

    update choosing-passengers.js : à l'ouverture de la page choosing-passengers.hmtl, le bloc passengers-nb-selector modifie son affichage en fonction du nombre de passagers précédemment sélectionnés

--- src/js/choosing-passengers.js
+++ src/js/choosing-passengers.js
@@ -30,21 +30,44 @@ window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', fun
     }
 });
 
-
 /******************************************************/
-/*            Animation des plus-minus                */
+/*            Initialisation des plus-minus           */
 /******************************************************/
 // Récupération des éléments
 const plus = document.querySelector('.bi-plus-circle');
 const minus = document.querySelector('.bi-dash-circle');
 const passengers_nb = document.getElementById("passengers-nb");
+window.onload = function() {
+    if(localStorage.getItem("selectedPassengers"))
+    {
+        passengers_nb.innerHTML = localStorage.getItem("selectedPassengers");
+    }
+    else
+    {
+        passengers_nb.innerHTML = 1;
+    }
+    if(passengers_nb.innerHTML == 1)
+    {
+        minus.style.color = "var(--custom-primary-2)";
+    } else if (passengers_nb.innerHTML == 10)
+    {
+        plus.style.color = "var(--custom-primary-2)";
+        
+    } else {
+        minus.style.color = "var(--custom-primary)";
+        plus.style.color = "var(--custom-primary)";
+    }
+}
+/******************************************************/
+/*            Animation des plus-minus                */
+/******************************************************/
 document.getElementById("plus").addEventListener("click", function() {
     if(passengers_nb.innerHTML < 10)
     {
         minus.style.color = "var(--custom-primary)";
         if(passengers_nb.innerHTML == 9)
         {
-            plus.style.color = "var(--custom-primary-5)";
+            plus.style.color = "var(--custom-primary-2)";
         }
         passengers_nb.innerHTML++;
         localStorage.setItem("selectedPassengers", passengers_nb.innerHTML);
@@ -55,7 +78,7 @@ document.getElementById("minus").addEventListener("click", function() {
     {
         if(passengers_nb.innerHTML == 2)
         {
-            minus.style.color = "var(--custom-primary-5)";
+            minus.style.color = "var(--custom-primary-2)";
         }
         plus.style.color = "var(--custom-primary)";
         passengers_nb.innerHTML--;
