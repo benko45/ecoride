847abe07b2eb755146d9b3c24a9f958d97749dc5
Benoît Vicente <benoit.vicente@hotmail.fr>
2025-02-27 23:18:13 +0100

    création du module applyTheme.js ; intégration dans les autres fichiers

--- src/js/index.js
+++ src/js/index.js
@@ -1,48 +1,9 @@
 "use strict";
 
+import { applyTheme } from './apply-theme.js';
 /******************************************************/
-/*            Gestion des Thèmes                      */
 /******************************************************/
-
-// Variable pour stocker la sélection de l'utilisateur
-var selectedTheme = localStorage.getItem("theme") || "auto";
-
-// Fonction pour appliquer le thème effectif
-function applyTheme(theme) {
-    // Déterminer le vrai thème utilisé (light ou dark) en cas de "auto"
-    var effectiveTheme = (theme === "auto")
-        ? (window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light")
-        : theme;
-
-    // **1️⃣ Met à jour Bootstrap (applique la couleur correcte)**
-    document.documentElement.setAttribute("theme", effectiveTheme);
-
-    // **2️⃣ Stocke le thème sélectionné pour l'affichage correct de l'icône**
-    document.documentElement.setAttribute("selected-theme", theme);
-
-    // Sauvegarder le thème dans le localStorage pour la prochaine fois
-    localStorage.setItem("theme", theme);
-}
-
-// Appliquer le thème immédiatement au chargement
-applyTheme(selectedTheme);
-
-// Gestion des clics sur le dropdown
-var dropdownItems = document.querySelectorAll('.dropdown-item');
-dropdownItems.forEach(function (item) {
-    item.addEventListener('click', function () {
-        selectedTheme = this.getAttribute('theme');
-        applyTheme(selectedTheme);
-    });
-});
-
-// **Écoute les changements du mode système en mode auto**
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function (event) {
-    if (selectedTheme === "auto") {
-        applyTheme("auto");
-    }
-});
-
+applyTheme();
 /******************************************************/
 /******************************************************/
 
@@ -358,6 +319,6 @@ document.getElementById("search").addEventListener("click", function() {
             ? window.location.href = "public/html/choosing-address.html"
             : selectedArrivalAddress === 'Arrivée'
                 ? window.location.href = "public/html/choosing-arrival-address.html"
-                : window.location.href = "public/html/choosing-traject.html";
+                : window.location.href = "public/html/search-result.html";
     }
 });
