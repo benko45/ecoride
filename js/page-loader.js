2d85322b4e43692249af639daddacc2e4a820c9c
Benoît Vicente <benoit.vicente@hotmail.fr>
2025-03-17 04:22:45 +0100

    test

--- js/page-loader.js
+++ js/page-loader.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
         if (target) {
             event.preventDefault();
             console.log("🔄 Clic sur un lien <a> détecté :", target.href);
-            console.log("🔄 Clic sur un lien <a> détecté : HREF.PATHNAME = ", new URL(target.href).pathnamef);
+            console.log("🔄 Clic sur un lien <a> détecté : HREF.PATHNAME = ", new URL(target.href).pathname);
             loadPage(new URL(target.href).pathname);
         }
     });
