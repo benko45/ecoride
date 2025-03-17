e20ef1a17dd1bfaaea7c1ea9c7de3f2af98024f2
Benoît Vicente <benoit.vicente@hotmail.fr>
2025-03-17 03:55:32 +0100

    test

--- js/functions.js
+++ js/functions.js
@@ -18,7 +18,7 @@ export const selectImage = () => {
     console.log("🖼️ selectImage() appelée !");
 
     // Définition du préfixe en fonction de l'environnement (GitHub Pages ou local)
-    const prefix = window.location.hostname === "benko45.github.io" ? "/ecoride" : "";
+    const prefix = window.location.pathname.startsWith("/ecoride") ? "/ecoride" : "";
 
     const imageSources = [
         {
