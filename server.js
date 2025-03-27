4d9802000bac3e60f08238057c01cab6ffe6eec6
benko45 <benoit.vicente@hotmail.fr>
2025-03-27 03:00:29 +0100

    idem

--- server.js
+++ server.js
@@ -9,36 +9,39 @@ app.use(helmet());
 // Configuration du Content-Security-Policy
 app.use(helmet.contentSecurityPolicy({
   directives: {
-    defaultSrc: ["'self'"], // Permet uniquement les ressources provenant du même domaine
-    scriptSrc: ["'self'", // Autorise les scripts venant du même domaine
-                "https://code.jquery.com/jquery-3.7.1.min.js",
-                "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js",
-                "https://cdn.jsdelivr.net/npm/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js",
-                "https://cdn.jsdelivr.net/npm/bootstrap-datepicker/dist/locales/bootstrap-datepicker.fr.min.js",
-                "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js",
-                "https://kit.fontawesome.com/06e14d9221.js",
-                "https://code.jquery.com/jquery-3.7.1.min.js",
-                "https://unpkg.com/trusted-types@3.0.4/dist/es6/trustedtypes.full.es6.js"], 
-    styleSrc: ["'self'", // Autorise les styles venant du même domaine
-                "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
-                "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
-                "https://cdn.jsdelivr.net/npm/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css",
-                "https://cdn.jsdelivr.net/npm/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css",
-                "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"], 
-    imgSrc: ["'self'"], // Autorise les images venant du même domaine
-    connectSrc: ["'self'", "https://nominatim.openstreetmap.org/"], // Ajout de la directive connectSrc pour les connexions XMLHttpRequest / WebSockets
-    fontSrc: ["'self'"], // Autorise les polices venant du même domaine
-    objectSrc: ["'none'"], // Bloque les objets (comme Flash)
-    upgradeInsecureRequests: [], // Force les connexions HTTP à se faire en HTTPS si possible
-    baseUri: ["'self'"], // Permet uniquement les liens de base venant du même domaine
-    formAction: ["'self'"], // Autorise uniquement les actions de formulaire vers le même domaine
-    frameAncestors: ["'self'"], // Bloque l'insertion du site dans un iframe
-    trustedTypes: ["default"], // Active Trusted Types
-    requireTrustedTypesFor: ["script"], //Bloque :  
-                                             // eval("...")
-                                             // new Function("...")
-                                             // setTimeout("...") (avec une string) 
-                                             // setInterval("...") (avec une string)
+    defaultSrc: ["'self'"],
+
+    scriptSrc: [
+      "'self'",
+      "https://code.jquery.com/jquery-3.7.1.min.js",
+      "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js",
+      "https://cdn.jsdelivr.net/npm/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js",
+      "https://cdn.jsdelivr.net/npm/bootstrap-datepicker/dist/locales/bootstrap-datepicker.fr.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js",
+      "https://kit.fontawesome.com/06e14d9221.js",
+      "https://unpkg.com/trusted-types@3.0.4/dist/es6/trustedtypes.full.es6.js"
+    ],
+
+    styleSrc: [
+      "'self'",
+      "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
+      "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
+      "https://cdn.jsdelivr.net/npm/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css",
+      "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
+    ],
+
+    imgSrc: ["'self'"],
+    connectSrc: ["'self'", "https://nominatim.openstreetmap.org/"],
+    fontSrc: ["'self'"],
+    objectSrc: ["'none'"],
+    upgradeInsecureRequests: [], // active HTTPS automatique si possible
+    baseUri: ["'self'"],
+    formAction: ["'self'"],
+    frameAncestors: ["'self'"],
+
+    trustedTypes: ["default"],
+    requireTrustedTypesFor: ["script"]
+  }
 }));
 
 // Liste d'IP autorisées
