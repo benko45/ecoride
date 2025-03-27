18816e3086213a173fa297007d3d78d53cf613e4
benko45 <benoit.vicente@hotmail.fr>
2025-03-27 02:13:00 +0100

    test pour implémenter window.TrustedTypes pour la CSP

--- security.js
+++ security.js
@@ -12,18 +12,19 @@ if (window.TrustedTypes) {
         return input;  // Construit des scripts sécurisés
       }
     });
-  } else {
-    // Crée une politique par défaut pour sécuriser le HTML et les scripts
-    policy = {
-      createHTML: (input) => {
-        // Tu peux ajouter ici des vérifications supplémentaires sur le contenu HTML avant de l'accepter
-        return input;  // Construit des chaînes HTML sécurisées
-      },
-      createScript: (input) => {
-        // Vérifie ou sécurise les scripts avant de les accepter
-        return input;  // Construit des scripts sécurisés
-      }
-    };
   }
+  //  else {
+  //   // Crée une politique par défaut pour sécuriser le HTML et les scripts
+  //   policy = {
+  //     createHTML: (input) => {
+  //       // Tu peux ajouter ici des vérifications supplémentaires sur le contenu HTML avant de l'accepter
+  //       return input;  // Construit des chaînes HTML sécurisées
+  //     },
+  //     createScript: (input) => {
+  //       // Vérifie ou sécurise les scripts avant de les accepter
+  //       return input;  // Construit des scripts sécurisés
+  //     }
+  //   };
+  // }
   
   export { policy };
