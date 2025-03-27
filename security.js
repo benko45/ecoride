c71d41a2784c90b950ce2a79e777307302b0154f
benko45 <benoit.vicente@hotmail.fr>
2025-03-27 04:30:18 +0100

    idem

--- security.js
+++ security.js
@@ -1,30 +1,14 @@
-let policy;
-
-if (window.TrustedTypes) {
-    // Crée une politique par défaut pour sécuriser le HTML et les scripts
-    policy = TrustedTypes.createPolicy('default', {
-      createHTML: (input) => {
-        // Tu peux ajouter ici des vérifications supplémentaires sur le contenu HTML avant de l'accepter
-        return input;  // Construit des chaînes HTML sécurisées
-      },
-      createScript: (input) => {
-        // Vérifie ou sécurise les scripts avant de les accepter
-        return input;  // Construit des scripts sécurisés
-      }
-    });
-  }
-  //  else {
-  //   // Crée une politique par défaut pour sécuriser le HTML et les scripts
-  //   policy = {
-  //     createHTML: (input) => {
-  //       // Tu peux ajouter ici des vérifications supplémentaires sur le contenu HTML avant de l'accepter
-  //       return input;  // Construit des chaînes HTML sécurisées
-  //     },
-  //     createScript: (input) => {
-  //       // Vérifie ou sécurise les scripts avant de les accepter
-  //       return input;  // Construit des scripts sécurisés
-  //     }
-  //   };
-  // }
-  
-  export { policy };
+if (typeof trustedTypes !== 'undefined') {
+  window.policy = trustedTypes.createPolicy('default', {
+    createHTML: (input) => input,
+    createScriptURL: (input) => input,
+    //  createScript: (input) => input : pas encore assez généralement supporté
+  });
+} else {
+  // Fallback pour les navigateurs sans Trusted Types
+  window.policy = {
+    createHTML: (input) => input,
+    createScriptURL: (input) => input,
+    //  createScript: (input) => input
+  };
+}
