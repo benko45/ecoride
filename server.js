e66cb072da6e4dff9642cd926bfd196eb0039d10
Benoît Vicente <benoit.vicente@hotmail.fr>
2025-03-30 18:30:47 +0200

    implémentation filtrage ip

--- server.js
+++ server.js
@@ -128,7 +128,11 @@ app.use((req, res, next) => {
   if (userAgent.includes('ZAP')) return next();
   const isAllowedIpv6 = allowedIpv6Prefixes.some(prefix => clientIp.startsWith(prefix));
   if (!allowedIps.includes(clientIp) && !isAllowedIpv6) {
-    return res.status(403).sendFile(path.join(__dirname, '403.html'));
+      return res.status(403).render('403', {
+        layout: false,
+        nonce: res.locals.nonce,
+        title: "Accès refusé"
+      });
   }
 
   next();
